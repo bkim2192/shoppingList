6ffddb2d58c372c95503ae1673c71a910d323e18
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-02-08 14:55:17 -0600

    Initial Commit

---
+++ shoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shoppingList
+//
+//  Created by Brandon Kim on 2/8/19.
+//  Copyright © 2019 Brandon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
