79bb782f884f4775bbe12009089ec8576da3b4dd
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-02-11 14:57:49 -0600

    Added table view, cell (1 type), Ibactions and outlets

--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -11,12 +11,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var newItemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func newItemButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
